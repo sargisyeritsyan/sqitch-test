62be2e9daf6fcb0346009d1b1f9de035a1cd17b9
sargisyeritsyan <sargisy@doublecoconut.com>
2022-09-29 13:56:50 +0400

    'Tag the database with v1.0.0-dev1.'

---
+++ verify/insert_user.sql
@@ -0,0 +1,7 @@
+-- Verify flipr:insert_user on pg
+
+BEGIN;
+
+SELECT has_function_privilege('flipr.insert_user(text, text)', 'execute');
+
+ROLLBACK;
---
+++ verify/users.sql
@@ -0,0 +1,8 @@
+-- Verify flipr:users on pg
+
+BEGIN;
+
+SELECT nickname, password, timestamp
+  FROM flipr.users
+ WHERE FALSE;
+ROLLBACK;
